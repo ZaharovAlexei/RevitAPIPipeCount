d5576d6789f5aeb18e28a28d237b1f60df1a96fb
Алексей Захаров <zakharov.nnov@gmail.com>
2022-02-02 00:33:12 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIPipeCount
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pipes = new FilteredElementCollector(doc)
+                .OfClass(typeof(Pipe))
+                .Cast<Pipe>()
+                .ToList();
+
+            TaskDialog.Show("Pipe info", $"Количество труб в проекте: {pipes.Count.ToString()}");
+
+            return Result.Succeeded;
+        }
+    }
+}
